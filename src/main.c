f6b3a4c3b5285845cdffd0ba831b979e953b9ead
David Balzer <dmbalzer@gmail.com>
2025-02-19 10:38:09 -0500

    started objects

--- src/main.c
+++ src/main.c
@@ -9,37 +9,60 @@ typedef enum {
 	QUIT,
 } State;
 
+void process_file_drop(void);
 void draw_main_menu(void);
 
 static State state = RUN;
 
 int main(void)
 {
+	/* Initialization */
 	InitWindow(960, 640, "Modbus Viewer");
 	SetTargetFPS(30);
 	SetExitKey(KEY_NULL);
-
+	
+	/* Main loop */
 	while ( state != QUIT ) {
+		/* Process events */
 		if ( WindowShouldClose() ) state = QUIT;
+		if ( IsFileDropped() ) process_file_drop();
 		
-		if ( IsFileDropped() ) {
-			FilePathList fpl = LoadDroppedFiles();
-			for ( int i = 0; i < fpl.count; i++ ) {  }
-			UnloadDroppedFiles(fpl);
-		}
+		/* Update app */
 		
+		/* Draw app */
 		BeginDrawing();
 		ClearBackground(WHITE);
 
 		draw_main_menu();
 		EndDrawing();
 	}
-		
+	
+	/* Cleanup */
 	CloseWindow();
 	return 0;
 }
 
+void process_file_drop(void)
+{
+	FilePathList fpl = LoadDroppedFiles();
+	for ( int i = 0; i < fpl.count; i++ ) {
+		
+	}
+	UnloadDroppedFiles(fpl);
+}
+
 void draw_main_menu(void)
 {
+	/* Button dimensions */
+	int w = 60;
+	int h = 20;
+	int pad = 4;
 	
+	/* Panel */
+	Rectangle bounds = (Rectangle){ 0, 0, GetScreenWidth(), h+pad*2 };
+	GuiPanel(bounds, NULL);
+	
+	/* Quit Button */
+	Rectangle bounds_btn = (Rectangle){ GetScreenWidth() - w - pad, pad, w, h };
+	if ( GuiButton(bounds_btn, "Quit") ) state = QUIT;
 }
--- src/objs.h
+++ src/objs.h
@@ -3,4 +3,22 @@
 
 #include <raylib.h>
 
+typedef enum {
+	STATIC_IMG,
+	STATIC_TXT,
+	BIT_IMG,
+	BIT_TXT,
+	REG_IMG,
+	REG_TXT,
+	MB_CTX,
+} ObjType;
+
+typedef struct _object Object;
+struct _object {
+	Vector2 position;
+	ObjType type;
+	void* obj;
+	Object* next;
+};
+
 #endif // OBJS_INCLUDED
