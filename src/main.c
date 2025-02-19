8ddac5a342f5aa3d05fbb4487f1b3eedc43f5ba3
David Balzer <dmbalzer@gmail.com>
2025-02-19 08:21:45 -0500

    added app states

--- src/main.c
+++ src/main.c
@@ -1,12 +1,18 @@
 #define RAYGUI_IMPLEMENTATION
 #include <raygui.h>
+#include <string.h>
 #include "imgs.h"
 #include "objs.h"
 
-bool quit = false;
-bool show_new_obj = false;
-bool show_edit_obj = false;
-bool show_img_list = false;
+#define STATE_QTY 4
+typedef enum {
+	QUIT,
+	NEW_OBJ,
+	EDIT_OBJ,
+	IMG_LIST,
+} States;
+
+bool state[4] = { 0 };
 
 void draw_main_menu(void);
 
@@ -14,8 +20,10 @@ int main(void)
 {
 	InitWindow(960, 640, "Modbus Viewer");
 	SetTargetFPS(30);
-	while ( !quit ) {
-		if ( WindowShouldClose() ) quit = true;
+	SetExitKey(KEY_NULL);
+
+	while ( !state[QUIT] ) {
+		if ( WindowShouldClose() ) state[QUIT]= true;
 		
 		if ( IsFileDropped() ) {
 			FilePathList fpl = LoadDroppedFiles();
@@ -26,8 +34,8 @@ int main(void)
 		BeginDrawing();
 		ClearBackground(WHITE);
 		draw_objs();
-		if ( show_new_obj ) show_new_obj = !draw_new_obj();
-		if ( show_img_list ) show_img_list = !draw_img_list();
+		if ( state[NEW_OBJ] ) state[NEW_OBJ]   = !draw_new_obj();
+		if ( state[IMG_LIST] ) state[IMG_LIST] = !draw_img_list();
 		draw_main_menu();
 		EndDrawing();
 	}
@@ -43,9 +51,11 @@ void draw_main_menu(void)
 	Rectangle bounds = (Rectangle){ 0, 0, GetScreenWidth(), 28 };
 	GuiPanel(bounds, NULL);
 	Rectangle btn_bounds = (Rectangle){ 4, 4, 80, 20 };
-	if ( GuiButton(btn_bounds, "New Object") ) show_new_obj = true;
+	if ( GuiButton(btn_bounds, "New Object") )  { memset(state, 0, sizeof(bool)*STATE_QTY); state[NEW_OBJ] = true; }
 	btn_bounds.x += btn_bounds.width + 4;
-	if ( GuiButton(btn_bounds, "Edit Object") ) show_edit_obj = true;
+	if ( GuiButton(btn_bounds, "Edit Object") ) { memset(state, 0, sizeof(bool)*STATE_QTY); state[EDIT_OBJ] = true; }
 	btn_bounds.x += btn_bounds.width + 4;
-	if ( GuiButton(btn_bounds, "Image List") ) show_img_list = true;
+	if ( GuiButton(btn_bounds, "Image List") )  { memset(state, 0, sizeof(bool)*STATE_QTY); state[IMG_LIST] = true; }
+	btn_bounds.x = GetScreenWidth()-btn_bounds.width-4;
+	if ( GuiButton(btn_bounds, "Quit") )        { memset(state, 0, sizeof(bool)*STATE_QTY); state[QUIT] = true; }
 }
