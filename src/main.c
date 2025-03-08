760abf3579949747bb8201d0fe572526ff3582ae
David Balzer <dmbalzer@gmail.com>
2025-03-08 08:45:11 -0500

    added drop pngs

--- src/main.c
+++ src/main.c
@@ -3,22 +3,47 @@
 #include "stb_ds.h"
 #include "mb.h"
 
+Texture* textures = NULL;
+
 int main(void)
 {
 	InitWindow(960, 640, "Modbus Viewer");
 	SetTargetFPS(30);
+	Font font = LoadFontEx("assets/Roboto-Regular.ttf", 18, NULL, 0);
 	void* ctx = mb_ctx_new("192.168.0.12", 1502);
 	bool status = mb_ctx_connect(ctx);
-	Font font = LoadFontEx("assets/Roboto-Regular.ttf", 18, NULL, 0);
+
 	while ( !WindowShouldClose() ) {
+		
+		if ( IsFileDropped() ) {
+			FilePathList fpl = LoadDroppedFiles();
+
+			for ( int i = 0; i < fpl.count; i++ ) {
+				if ( IsFileExtension(fpl.paths[i], ".png") ) {
+					arrput(textures, LoadTexture(fpl.paths[i]));
+				}
+			}
+
+			UnloadDroppedFiles(fpl);
+		}
+
 		BeginDrawing();
 		ClearBackground(RAYWHITE);
+
 		DrawTextEx(font, TextFormat("%s", status ? "ONLINE" : "OFFLINE"), (Vector2){ 16, 16 }, 18, 0, status ? DARKGREEN : DARKGRAY);
+		for ( int i = 0; i < arrlen(textures); i++ ) {
+			DrawTexture(textures[i], 16, 16, WHITE);
+		}
+		
 		EndDrawing();
 	}
-	UnloadFont(font);
+
+	for ( int i = 0; i < arrlen(textures); i++ ) {
+		UnloadTexture(textures[i]);
+	}
 	mb_ctx_close(ctx);
 	mb_ctx_free(ctx);
+	UnloadFont(font);
 	CloseWindow();
 	return 0;
 }
